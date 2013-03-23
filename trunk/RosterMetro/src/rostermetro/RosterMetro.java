9639bf2800e564e440c0da8bdbb0a1c3d7cbecab
miguelgonzalezgomez@gmail.com <miguelgonzalezgomez@gmail.com@99e7c1b8-792a-a383-46de-740de850d7ee>
2013-03-23 11:07:05 +0000

    Agregada información de la estructura de la aplicación

--- trunk/RosterMetro/src/rostermetro/RosterMetro.java
+++ trunk/RosterMetro/src/rostermetro/RosterMetro.java
@@ -11,7 +11,46 @@ public class RosterMetro {
          * Procesador fichero KML y Líneas Metro
          */
         
+        /*
+            PlanoDeMetro
+            * Nombre
+            * Listado de lineas
+            * ObtenerLíneas(Dada parada)
+                Lineas
+                * Nombre
+                * Listado de paradas
+                    Parada
+                    * Nombre
+                    * Coordenadas
+        */
         
+        /*
+         * Algoritmo de búsqueda
+            * Parada inicial
+            * Parada final
+            * Arcos lo forman las paradas de las líneas
+         */
+        
+        /*
+         * Información a parsear (quitando datos que no vamos a leer)
+         * 
+         * <Folder> <- Agrupación de paradas de metro
+            <name>Madrid Metro Line 1</name>
+            <Placemark> <- Parada
+              <name>Plaza de Castilla Metro</name>
+              <description>Lines 1, 9, 10. Bus terminal</description>
+              <Point id="khPoint540">
+                <coordinates>-3.689170797285812,40.46674869438549,0</coordinates>
+              </Point>
+            </Placemark>
+            <Placemark>
+              <name>Valdeacederas Metro</name>
+              <description>Line 1</description>
+              <Point>
+                <coordinates>-3.694774074060931,40.46447773263045,0</coordinates>
+              </Point>
+            </Placemark>
+         */
     }
 
     
