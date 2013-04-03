5128ee19d561b40efc2f2f934ff0f15811be912e
jaimebarez <jaimebarez@gmail.com>
2013-04-03 19:44:58 +0000

    Estructuras de datos terminadas

--- RosterMetro/src/rostermetro/RosterMetro.java
+++ RosterMetro/src/rostermetro/RosterMetro.java
@@ -6,7 +6,14 @@ import com.keithpower.gekmlib.Kml;
 import com.keithpower.gekmlib.Placemark;
 import java.io.File;
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 import org.xml.sax.SAXException;
+import rostermetro.domain.Linea;
+import rostermetro.domain.Parada;
 import rostermetro.domain.PlanoMetro;
 import rostermetro.parserKML.ParserXML;
 
@@ -19,12 +26,23 @@ public class RosterMetro {
     public static void main(String[] args) throws IOException, SAXException {
 
         PlanoMetro metroMadrid = ParserXML.parse(RosterMetro.class.getResourceAsStream("metroMadrid.kml"));
-        System.out.println(metroMadrid);
+        //System.out.println(metroMadrid);
+        Map<Parada, List<Linea>> paradaYLineas = metroMadrid.getParadaYLineas();
+        Set<Entry<Parada, List<Linea>>> entrySet = paradaYLineas.entrySet();
+        Iterator<Entry<Parada, List<Linea>>> iterator = entrySet.iterator();
+        while (iterator.hasNext()) {
+            Entry<Parada, List<Linea>> next = iterator.next();
+            Parada key = next.getKey();
+            System.out.println(key.getNombre());
+            List<Linea> value = next.getValue();
+            for (Linea linea : value) {
+                System.out.println("-" + linea.getNombre());
+            }
+        }
 
         /*
          * Procesador fichero KML y Líneas Metro
          */
-
         /*
          PlanoDeMetro
          * Nombre
@@ -37,14 +55,12 @@ public class RosterMetro {
          * Nombre
          * Coordenadas
          */
-
         /*
          * Algoritmo de búsqueda
          * Parada inicial
          * Parada final
          * Arcos lo forman las paradas de las líneas
          */
-
         /*
          * Información a parsear (quitando datos que no vamos a leer)
          * 
--- RosterMetro/src/rostermetro/domain/PlanoMetro.java
+++ RosterMetro/src/rostermetro/domain/PlanoMetro.java
@@ -4,9 +4,12 @@
  */
 package rostermetro.domain;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import rostermetro.Utilidades;
 
 /**
@@ -23,17 +26,30 @@ public class PlanoMetro {
         this.nombre = nombre;
         this.lineas = lineas;
         paradaYLineas = new HashMap<>();
+        for (Linea linea : lineas) {
+            for (Parada parada : linea.getParadas()) {
+                if (paradaYLineas.containsKey(parada)) {
+                    paradaYLineas.get(parada).add(linea);
+                } else {
+                    ArrayList<Linea> lineasList = new ArrayList<>();
+                    lineasList.add(linea);
+                    paradaYLineas.put(parada, lineasList);
+                }
+            }
+        }
     }
 
     @Override
     public String toString() {
         StringBuilder str = new StringBuilder();
         Utilidades.appendLine(str, nombre);
-        for(Linea linea : lineas){
+        for (Linea linea : lineas) {
             Utilidades.appendLine(str, linea.toString());
         }
         return str.toString();
     }
 
-    
+    public Map<Parada, List<Linea>> getParadaYLineas() {
+        return paradaYLineas;
+    }
 }
