05be0cbd4bbb6b2ac0012bf06990e9287e6accde
miguelgonzalezgomez <miguelgonzalezgomez@gmail.com>
2013-04-17 19:10:27 +0000

    Versión MetroSwing

---
+++ RosterMetroSwing/src/rostermetroswing/Controlador.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rostermetroswing;
+
+/**
+ *
+ * @author Roster
+ */
+public class Controlador {
+    
+}
