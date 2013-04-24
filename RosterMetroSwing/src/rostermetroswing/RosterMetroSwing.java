60cb6326e85ae0b159cc3dc4e7b6c931fa4bf7bc
jaimebarez <jaimebarez@gmail.com>
2013-04-24 17:52:39 +0000

    Mejorado

--- RosterMetroSwing/src/rostermetroswing/RosterMetroSwing.java
+++ RosterMetroSwing/src/rostermetroswing/RosterMetroSwing.java
@@ -1,68 +1,44 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package rostermetroswing;
 
-import java.awt.BorderLayout;
-import java.awt.Container;
-import java.awt.FlowLayout;
-import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.IOException;
-import java.net.URL;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.imageio.ImageIO;
-import javax.swing.DefaultComboBoxModel;
-import javax.swing.ImageIcon;
-import javax.swing.JComboBox;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.JTextArea;
-import javax.swing.SwingUtilities;
-import javax.swing.table.AbstractTableModel;
-import javax.swing.table.TableModel;
-import org.xml.sax.SAXException;
-import rostermetro.RosterMetro;
-import rostermetro.domain.Coordenada;
-import rostermetro.domain.Parada;
-import rostermetro.domain.PlanoMetro;
-import rostermetro.domain.Ruta;
-import rostermetro.parserKML.ParserXML;
-import rostermetroswing.components.PlanoMetroDibujo;
-import rostermetroswing.model.TablaRutaModel;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.util.*;
+import java.util.logging.*;
+import javax.swing.*;
+import org.xml.sax.*;
+import rostermetro.*;
+import rostermetro.busqueda.*;
+import rostermetro.domain.*;
+import rostermetro.parsers.*;
+import rostermetroswing.components.*;
+import rostermetroswing.model.*;
 
 /**
  *
- * @author paracaidista
+ * @author Jaime Bárez y Miguel González
  */
 public class RosterMetroSwing extends JFrame {
 
-    private PlanoMetro planoMetro;
-    private JComboBox origenComboBox;
-    private JComboBox destinoComboBox;
-    private JTable tableMuestraRutas;
-    private PlanoMetroDibujo planoMetroDibujo;
-    private TablaRutaModel tablaRutaModel;
-    private Parada[] paradas;
-    
+    private Plano plano;
+    private JComboBox<Parada> origenCBox;
+    private JComboBox<Parada> destinoCBox;
+    private JComboBox<Utilidades.PlanoAlmacenado> planoComboBox;
+    private JTable rutaJTable;
+    private PlanoGoogleMaps planoMetroDibujo;
+    private PlanoParser planoParser;
+
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
                 RosterMetroSwing buscaParadasSimple =
-                        new RosterMetroSwing("Roster metro");
+                        new RosterMetroSwing("Roster Metro");
                 buscaParadasSimple.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                buscaParadasSimple.setBounds(100, 100, 1000, 500);
+
+                buscaParadasSimple.pack();
+                buscaParadasSimple.setLocationRelativeTo(null);
+
                 buscaParadasSimple.setVisible(true);
             }
         });
@@ -70,19 +46,95 @@ public class RosterMetroSwing extends JFrame {
 
     public RosterMetroSwing(String nombreAplicacion) {
         super(nombreAplicacion);
+        planoParser = new PlanoKMLParser();
 
+        initComponenets();
+
+        initListeners();
+
+        initInterfaz();
+        setPlano((Utilidades.PlanoAlmacenado) planoComboBox.getSelectedItem());
+    }
+
+    private void initComponenets() {
+
+        planoComboBox = new JComboBox<>(new DefaultComboBoxModel<>(Utilidades.getPlanosAlmacenados()));
+        origenCBox = new JComboBox<>();
+        destinoCBox = new JComboBox<>();
+        rutaJTable = new JTable();
+
+        planoMetroDibujo = new PlanoGoogleMaps();
+    }
+
+    private void initListeners() {
+        ActionListener muestraRuta = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                mostrarRuta();
+            }
+        };
+        planoComboBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                setPlano((Utilidades.PlanoAlmacenado) planoComboBox.getSelectedItem());
+
+            }
+        });
+        origenCBox.addActionListener(muestraRuta);
+        destinoCBox.addActionListener(muestraRuta);
+
+    }
+
+    private void initInterfaz() {
+        Container container = getContentPane();
+        JPanel principal = new JPanel(new BorderLayout());
+        JPanel panelIzquierda = new JPanel(new BorderLayout());
+        panelIzquierda.add(new JScrollPane(rutaJTable), BorderLayout.CENTER);
+        principal.add(panelIzquierda, BorderLayout.WEST);
+
+        JPanel panelSuperiorCentro = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        panelSuperiorCentro.add(origenCBox);
+        panelSuperiorCentro.add(destinoCBox);
+        panelSuperiorCentro.add(planoComboBox);
+
+        JPanel panelCentro = new JPanel(new BorderLayout());
+        panelCentro.add(panelSuperiorCentro, BorderLayout.NORTH);
+        panelCentro.add(planoMetroDibujo, BorderLayout.CENTER);
+
+        principal.add(panelCentro, BorderLayout.CENTER);
+        container.add(principal);
+    }
+
+    private void mostrarRuta() {
+        final Parada pInicial = (Parada) origenCBox.getSelectedItem();
+        final Parada pFinal = (Parada) destinoCBox.getSelectedItem();
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+
+                Ruta ruta = new BusquedaRuta().calcularRuta(pInicial, pFinal);
+                if (ruta != null) {
+                    rutaJTable.setModel(new TablaRutaModel(ruta));
+                    planoMetroDibujo.pintarRuta(ruta);
+                } else {
+                    rutaJTable.setModel(new TablaRutaModel(null));
+                    planoMetroDibujo.pintarRuta(null);
+                    JOptionPane.showMessageDialog(
+                            null,
+                            "No route",
+                            "No hay ruta",
+                            JOptionPane.INFORMATION_MESSAGE);
+                }
+            }
+        });
+    }
+
+    private void setPlano(Utilidades.PlanoAlmacenado parse) {
         try {
-            planoMetro = ParserXML.parse(RosterMetro.class.
-                    getResourceAsStream("metroMadrid.kml"));
+            plano = planoParser.parse(Utilidades.getPlanoAsStream(parse));
+        } catch (IOException | SAXException ex) {
 
-            initComponenets();
-            
-            initListeners();
-            
-            initInterfaz();
-
-            mostrarRuta();
-        } catch (Exception ex) {
             JOptionPane.showMessageDialog(
                     null,
                     "Error",
@@ -90,82 +142,27 @@ public class RosterMetroSwing extends JFrame {
                     JOptionPane.ERROR_MESSAGE);
 
             Logger.getLogger(RosterMetroSwing.class.getName()).log(Level.SEVERE, null, ex);
-
-
         }
+        setModels();
+        mostrarRuta();
+        pack();
     }
-    
 
-
-    private void initComponenets() {
-        Collection<Parada> coleccionParadas = planoMetro.getParadas();
-        paradas = new Parada[coleccionParadas.size()];
+    private void setModels() {
+        Collection<Parada> coleccionParadas = plano.getParadas();
+        Parada[] paradas = new Parada[coleccionParadas.size()];
         paradas = coleccionParadas.toArray(paradas);
-        
-        Arrays.sort(paradas, new Comparator<Object>() {
+        //Orden alfabético
+        Arrays.sort(paradas, new Comparator<Parada>() {
             @Override
-            public int compare(Object o1, Object o2) {
-                Parada p1 = (Parada) o1;
-                Parada p2 = (Parada) o2;
-                return p1.getNombre().compareToIgnoreCase(p2.getNombre());
+            public int compare(Parada o1, Parada o2) {
+                return o1.getNombre().compareToIgnoreCase(o2.getNombre());
             }
         });
-        
-        
-        origenComboBox = new JComboBox(new DefaultComboBoxModel(paradas));
-        destinoComboBox = new JComboBox(new DefaultComboBoxModel(paradas));
-        
-        tablaRutaModel = new TablaRutaModel();
-        tableMuestraRutas = new JTable(tablaRutaModel);
-        
-        planoMetroDibujo = new PlanoMetroDibujo();
-    }
-    
-    private void initListeners() {
-        origenComboBox.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                mostrarRuta();
-            }
-        });
-        destinoComboBox.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                mostrarRuta();
-            }
-        });
-    }
-    
-    private void initInterfaz() {
-        Container container = getContentPane();
-        
-        JPanel panelIzquierda = new JPanel(new BorderLayout());
-        panelIzquierda.add(new JScrollPane(tableMuestraRutas), BorderLayout.CENTER);
-        container.add(panelIzquierda, BorderLayout.WEST);
-        
-        JPanel panelSuperiorCentro = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        panelSuperiorCentro.add(origenComboBox);
-        panelSuperiorCentro.add(destinoComboBox);
-        
-        JPanel panelCentro = new JPanel(new BorderLayout());
-        panelCentro.add(panelSuperiorCentro, BorderLayout.NORTH);
-        panelCentro.add(planoMetroDibujo, BorderLayout.CENTER);
-        
-        container.add(panelCentro, BorderLayout.CENTER);
-    }
 
-    private void mostrarRuta() {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                Parada pInicial = (Parada) origenComboBox.getSelectedItem();
-                Parada pFinal = (Parada) destinoComboBox.getSelectedItem();
-                final Ruta ruta = planoMetro.getRuta(pInicial, pFinal);
-                
-                tablaRutaModel.replaceRuta(ruta);
-                planoMetroDibujo.pintarRuta(ruta);
-            }
-        });
+        origenCBox.setModel(new DefaultComboBoxModel<>(paradas));
+        destinoCBox.setModel(new DefaultComboBoxModel<>(paradas));
+
+
     }
-    
 }
--- RosterMetroSwing/src/rostermetroswing/model/TablaRutaModel.java
+++ RosterMetroSwing/src/rostermetroswing/model/TablaRutaModel.java
@@ -1,16 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package rostermetroswing.model;
 
-import java.awt.Point;
-import java.util.HashMap;
-import java.util.Iterator;
 import javax.swing.table.AbstractTableModel;
-import rostermetro.domain.Parada;
-import rostermetro.domain.Ruta;
-import rostermetroswing.axiliares.HashCodeUtil;
+import rostermetro.busqueda.Ruta;
 
 /**
  *
@@ -18,135 +9,30 @@ import rostermetroswing.axiliares.HashCodeUtil;
  */
 public class TablaRutaModel extends AbstractTableModel {
 
-    private HashMap<Point, Parada> lookup;
-    private int rows;
-    private int columns;
-    private String headers[];
-
-    public TablaRutaModel() {
-        headers = new String[]{"Paradas"};
-        lookup = new HashMap<>();
-        
-        rows = lookup.size();
-        columns = headers.length;
-    }
+    private final Ruta ruta;
     
     public TablaRutaModel(Ruta ruta) {
-        super();
-        
-        //Rellenamos los datos
-        for (Iterator<Parada> it = ruta.getListadoParadas().iterator(); it.hasNext();) {
-            Parada parada = it.next();
-            
-            lookup.put(new Point(lookup.size(), 0), parada);
-            
-        }
-        
-        rows = lookup.size();
-        columns = headers.length;
+        this.ruta = ruta;
     }
     
-    @Override
-    public boolean equals(Object tablaRutaModel) {
-        boolean columnasIguales = true;
-        
-        if(tablaRutaModel instanceof TablaRutaModel) {
-            TablaRutaModel p = (TablaRutaModel) tablaRutaModel;
-
-            if(p.getColumnCount() == columns && p.getRowCount() == rows) {
-                for(int i=0 ; i<columns; i++) {
-                    if(!p.getColumnName(i).equals(headers[i])) {
-                        columnasIguales = false;
-                    }
-                }
-            }
-        }
-        
-        
-        if(columnasIguales) {
-            TablaRutaModel p = (TablaRutaModel) tablaRutaModel;
-            //Comprobamos si tienen los mismos datos
-            for(int i=0; i<rows; i++) {
-                for(int j=0; j<columns; j++) {
-                    Point point = new Point(i, j);
-                    //Si no es el mismo dato devolvemos false
-                    if(!lookup.get(point).equals(p.getValueAt(i, j))) {
-                        return false;
-                    }
-                }
-            }
-            
-            return true;
-        }
-        
-        return false;
-    }
-    
-    @Override
-    public int hashCode() {
-        int result = HashCodeUtil.SEED;
-        
-        result = HashCodeUtil.hash(result, rows);
-        result = HashCodeUtil.hash(result, columns);
-        result = HashCodeUtil.hash(result, headers);
-        for(int i=0; i<rows; i++) {
-            for(int j=0; j<columns; j++) {
-                result = HashCodeUtil.hash(result, lookup.get(new Point(i, j)));
-            }
-        }
-        
-        return result;
-    }
-
-    @Override
-    public int getColumnCount() {
-        return columns;
-    }
-
     @Override
     public int getRowCount() {
-        return rows;
-    }
-
-    @Override
-    public String getColumnName(int column) {
-        return headers[column];
-    }
-
-    @Override
-    public Object getValueAt(int row, int column) {
-        return lookup.get(new Point(row, column));
-    }
-
-    public void setValueAt(Parada parada, int row, int column) {
-        if ((rows < 0) || (columns < 0)) {
-            throw new IllegalArgumentException("Invalid row/column setting");
-        }
-        if ((row < rows) && (column < columns)) {
-            lookup.put(new Point(row, column), parada);
-        }
-        
-        super.fireTableDataChanged();
+        return ruta!=null?ruta.getListadoParadas().size():1;
     }
     
-    public void replaceRuta(Ruta ruta) {
-        lookup.clear();
-        
-        //Rellenamos los datos
-        for (Iterator<Parada> it = ruta.getListadoParadas().iterator(); it.hasNext();) {
-            Parada parada = it.next();
-            
-            lookup.put(new Point(lookup.size(), 0), parada);
-            
-        }
-        
-        rows = lookup.size();
-        
-        super.fireTableDataChanged();
-    }
-
     @Override
-    public boolean isCellEditable(int row, int col) {
-        return false;
+    public int getColumnCount() {
+        return 1;
+    }
+    
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        return ruta!=null?ruta.getListadoParadas().get(rowIndex):"No existe ruta";
+    }
+    
+    @Override
+    public String getColumnName(int column) {
+        
+        return "Ruta";
     }
 }
