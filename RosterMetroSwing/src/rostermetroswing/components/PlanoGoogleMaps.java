019b6eb34af67b42c0748c2617bfcffa34d2db38
jaimebarez <jaimebarez@gmail.com>
2013-04-24 19:11:37 +0000

    PlanoMetroThreads

--- RosterMetroSwing/src/rostermetroswing/components/PlanoGoogleMaps.java
+++ RosterMetroSwing/src/rostermetroswing/components/PlanoGoogleMaps.java
@@ -1,16 +1,15 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package rostermetroswing.components;
 
 import java.awt.Dimension;
 import java.awt.Image;
 import java.net.URL;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
 import rostermetro.busqueda.Ruta;
 import rostermetro.domain.Coordenada;
 import rostermetro.domain.Parada;
@@ -20,92 +19,103 @@ import rostermetro.domain.Parada;
  * @author Ceura
  */
 public class PlanoGoogleMaps extends JLabel {
-
-    private ThreadPintado threadPintado;
-    private Image imagenPlano;
+    
+    private ThreadPintado threadPintadoActual;
+    private ThreadPintado nuevoThreadPintado;
     private static final int W_GOOGLE = 640;
     private static final int H_GOOGLE = 480;
-
+    
     public PlanoGoogleMaps() {
-        threadPintado = new ThreadPintado(null, null);
-        imagenPlano = null;
+        threadPintadoActual = new ThreadPintado(null);
         setPreferredSize(new Dimension(W_GOOGLE, H_GOOGLE));
     }
-
-    /*@Override
-     public void paintComponent(Graphics graphics) {
-     super.paintComponent(graphics);
-     Graphics g = (Graphics) graphics.create();
-        
-     //Pintamos el fondo blanco
-     g.setColor(Color.WHITE);
-     g.fillRect(0, 0, getWidth(), getHeight());
-        
-     if(imagenPlano != null) {
-     g.drawImage(imagenPlano, 0, 0, getWidth(), getHeight(), null);
-     }
-     }*/
+    
     public void pintarRuta(Ruta ruta) {
-        threadPintado.parar();
-        threadPintado = new ThreadPintado(this, ruta);
-        threadPintado.start();
-    }
-
-    public void dibujarImageIcon(Image imagenIcon) {
-        this.imagenPlano = imagenIcon;
-
-        setIcon(new ImageIcon(imagenIcon));
-    }
-
-    class ThreadPintado extends Thread {
-
-        private boolean pintando = true;
-        private PlanoGoogleMaps planoMetroDibujo;
-        private Ruta ruta;
-        private Image imagenPlano;
-
-        public ThreadPintado(PlanoGoogleMaps planoMetroDibujo, Ruta ruta) {
-            this.planoMetroDibujo = planoMetroDibujo;
-            this.ruta = ruta;
-        }
-
-        public void parar() {
-            pintando = false;
-        }
-
-        @Override
-        public void run() {
-            List<Parada> listadoParadas = ruta.getListadoParadas();
-            Parada pInicial = null;
-            Parada pFinal = null;
-            if (listadoParadas.size() > 0) {
-                pInicial = listadoParadas.get(0);
-            }
-            if (listadoParadas.size() > 1) {
-                pFinal = listadoParadas.get(listadoParadas.size() - 1);
-            } else {
-                pFinal = pInicial;
-            }
-
-            if (pFinal == null || pInicial == null) {
-                parar();
-            }
-
-            if (pintando) {
-                imagenPlano = getMap(pInicial, pFinal, W_GOOGLE, H_GOOGLE);
-
-                //Hack, algunas veces no lee a la primera vez
-                if (imagenPlano == null) {
-                    imagenPlano = getMap(pInicial, pFinal, W_GOOGLE, H_GOOGLE);
+        nuevoThreadPintado = new ThreadPintado(ruta);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    threadPintadoActual.join();
+                    threadPintadoActual = nuevoThreadPintado;
+                    threadPintadoActual.start();
+                } catch (Exception ex) {
+                    //Logger.getLogger(PlanoGoogleMaps.class.getName()).log(Level.SEVERE, null, ex);
                 }
             }
-
-            if (pintando) {
-                planoMetroDibujo.dibujarImageIcon(imagenPlano);
+        }).start();
+    }
+    
+    public void dibujarImageIcon(final Image imagenIcon) {
+        
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                if (imagenIcon != null) {
+                    setIcon(new ImageIcon(imagenIcon));
+                } else {
+                    setIcon(Imagen ruta inexistente);//ToDo: @miguel icono ruta inexistente
+                }
             }
+        });
+    }
+    
+    class ThreadPintado extends Thread {
+        
+        private boolean pintando = true;
+        private Ruta ruta;
+        
+        public ThreadPintado(Ruta ruta) {
+            this.ruta = ruta;
+        }
+        
+        public void parar() {
+            setPintando(false);
+        }
+        
+        @Override
+        public void run() {
+            if (ruta != null) {
+                List<Parada> listadoParadas = ruta.getListadoParadas();
+                Parada pInicial = null;
+                Parada pFinal = null;
+                if (listadoParadas.size() > 0) {
+                    pInicial = listadoParadas.get(0);
+                }
+                if (listadoParadas.size() > 1) {
+                    pFinal = listadoParadas.get(listadoParadas.size() - 1);
+                } else {
+                    pFinal = pInicial;
+                }
+                
+                if (pFinal == null || pInicial == null) {
+                    parar();
+                }
+                
+                if (isPintando()) {
+                    
+                    Image imagenPlano = getMap(pInicial, pFinal, W_GOOGLE, H_GOOGLE);
+
+                    //Hack, algunas veces no lee a la primera vez
+                    if (imagenPlano == null) {
+                        imagenPlano = getMap(pInicial, pFinal, W_GOOGLE, H_GOOGLE);
+                    }
+                    if (isPintando()) {
+                        if(imagenPlano==null){
+                            dibujarImageIcon(Imagen error de red);
+                        }else{
+                            dibujarImageIcon(imagenPlano);
+                        }
+                    }
+                }
+            } else {
+                dibujarImageIcon(null);
+            }
+            
+            
         }
         public static final int ZOOM = 12;
-
+        
         private Image getMap(double latorigen, double lonOrigen, double latDestino, double lonDestino, int width, int height) {
             Image img = null;
             try {
@@ -119,14 +129,35 @@ public class PlanoGoogleMaps extends JLabel {
                         url));
             } catch (Exception ex) {
                 System.out.println("Error!" + ex);
+                //@ToDo: devolver imagen error de red
             }
             return img;
         }
-
+        
         private Image getMap(Parada pInicial, Parada pFinal, int width, int height) {
             Coordenada cInicial = pInicial.getCoordenada();
             Coordenada cfiNal = pFinal.getCoordenada();
             return getMap(cInicial.getLatitud(), cInicial.getLongitud(), cfiNal.getLatitud(), cfiNal.getLongitud(), width, height);
+        }
+        final Object sinc = new Object();
+
+        /**
+         * @return the pintando
+         */
+        public boolean isPintando() {
+            synchronized (sinc) {
+                return pintando;
+            }
+            
+        }
+
+        /**
+         * @param pintando the pintando to set
+         */
+        public void setPintando(boolean pintando_) {
+            synchronized (sinc) {
+                this.pintando = pintando_;
+            }
         }
     }
 }
