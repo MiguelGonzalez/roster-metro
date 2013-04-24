621dc0af1311b0c397fc4a1628d207b0665c956a
jaimebarez@gmail.com <jaimebarez@gmail.com@99e7c1b8-792a-a383-46de-740de850d7ee>
2013-04-24 17:55:32 +0000

    Mejorado

--- trunk/RosterMetro/src/rostermetro/busqueda/FilaAAsterisco.java
+++ trunk/RosterMetro/src/rostermetro/busqueda/FilaAAsterisco.java
@@ -1,10 +1,7 @@
 package rostermetro.busqueda;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
 import rostermetro.busqueda.BusquedaRuta.TipoRuta;
 import rostermetro.domain.Parada;
 
