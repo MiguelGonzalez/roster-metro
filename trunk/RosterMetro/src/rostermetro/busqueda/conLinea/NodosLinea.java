0a7d7c154cc10437fc0befa39a789f6564bc753c
miguelgonzalezgomez@gmail.com <miguelgonzalezgomez@gmail.com@99e7c1b8-792a-a383-46de-740de850d7ee>
2013-05-15 16:56:12 +0000

    Comentada clase NodosLinea

--- trunk/RosterMetro/src/rostermetro/busqueda/conLinea/NodosLinea.java
+++ trunk/RosterMetro/src/rostermetro/busqueda/conLinea/NodosLinea.java
@@ -5,7 +5,9 @@ import java.util.List;
 import rostermetro.domain.Linea;
 
 /**
- *
+ * Contenedor de las líneas por las que pasa una ruta, y si la línea es
+ * distinta a la anterior suma un transbordo.
+ * 
  * @author Jaime Bárez y Miguel González
  */
 public class NodosLinea {
@@ -13,12 +15,23 @@ public class NodosLinea {
     private final List<Linea> lineasRecorridas;
     private int numTransbordos;
 
+    /**
+     * Construye el objeto con una línea de inicio.
+     * @param miLinea 
+     */
     public NodosLinea(Linea miLinea) {
         lineasRecorridas = new ArrayList<>();
         lineasRecorridas.add(miLinea);
         numTransbordos = 0;
     }
 
+    /**
+     * Construye el objeto a partir de otro objeto NodosLinea. Otra manera
+     * de hacerse hubiera sido implementar el método clone() para obtener
+     * una copia del mismo objeto.
+     * 
+     * @param nodosLinea 
+     */
     public NodosLinea(NodosLinea nodosLinea) {
         lineasRecorridas = new ArrayList<>(nodosLinea.getLineasTotales());
         numTransbordos = nodosLinea.getTransbordos();
